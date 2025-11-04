550f88572de7509d2f30c5db71b1559589f617b2
Вадим Якубовский <151447813+Vadim-Yakubowskij@users.noreply.github.com>
2025-11-04 15:44:46 +0300

    wip

--- vue-sneak/src/App.vue
+++ vue-sneak/src/App.vue
@@ -1,30 +1,66 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <div class="app">
+        <post-form
+            @create="createPost"
+        />
+        <post-list 
+            :posts="posts"
+        />
+    </div>
 </template>
 
+<script>
+import PostForm from './components/PostForm.vue';
+import PostList from './components/PostList.vue';
+export default {
+    components:{
+        PostForm, PostList
+    },
+    data(){
+        return{
+            posts:[
+                {id:1, title: 'JS 1', body: 'Описание поста 1'},
+                {id:2, title: 'JS 2', body: 'Описание поста 2'},
+                {id:3, title: 'JS 3', body: 'Описание поста 3'},
+            ],
+        }
+    },
+
+    methods: {
+        createPost(post){
+           this.posts.push(post);
+        },
+        
+    }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+.app{
+    padding: 20px;
+}
+form{
+    display: flex;
+    flex-direction: column;
+}
+.input{
+    width: 100%;
+    border: 1px solid teal;
+    padding: 10px 15px;
+    margin-top: 15px;
+}
+.btn{
+    margin-top: 15px;
+    align-self: flex-end;
+    padding: 10px 15px;
+    background: none;
+    color: teal;
+    border: 1px solid teal;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- vue-sneak/src/main.js
+++ vue-sneak/src/main.js
@@ -1,5 +1,3 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
-createApp(App).use(router).mount('#app')
+import App from './App'
+createApp(App).mount('#app')
